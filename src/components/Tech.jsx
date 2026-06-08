40fb504ea6399ba488a058fe619b6bea04571256
Ashvin Shakya <ashvinshakya04@gmail.com>
2026-06-08 22:04:37 +0530

    Testing mobile images

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -9,7 +9,11 @@ const Tech = () => {
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
+          <img
+  src={technology.icon}
+  alt={technology.name}
+  className="w-20 h-20 object-contain"
+/>
         </div>
       ))}
     </div>
